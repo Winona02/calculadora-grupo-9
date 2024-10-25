3f9e87d893de1b81f57c222e825df4149bda95d7
Walter David Gomez <waltergomezdavid@gmail.com>
2024-10-25 20:46:41 -0300

    agrego importación de suma y multiplicación

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 
 const potencia = require('./potencia');
 const raiz = require('./raiz');
+const sumar = require('./sumar');
+const multiplicar = require('./multiplicar');
 
 console.log("El resultado de la potencia es: " + potencia(9,3));
 console.log("El resultado de la raíz es: " + raiz(5));
+console.log("El resultado de la suma es: " + sumar(10,80));
+console.log("El resultado de la multiplicación es: " + multiplicar(5,5));
