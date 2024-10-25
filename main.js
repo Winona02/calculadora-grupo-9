871168e12aff54823c10da05f89b62b59126135c
Winona Muñoz <munozwinona8@gmail.com>
2024-10-25 13:53:11 -0300

    Agrego archivo main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+
+const potencia = require('./potencia');
+const raiz = require('./raiz');
+
+console.log("El resultado de la potencia es: " + potencia(9,3));
+console.log("El resultado de la raíz es: " + raiz(5));
